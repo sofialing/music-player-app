fad46565c2c363b0829b5555315231e81b569028
Sofia Ling <sofialing@live.se>
2020-12-28 15:45:10 +0100

    Fixed viewport max-height for mobile.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// Mobile full height screen fix.
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+window.addEventListener('resize', () => {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
