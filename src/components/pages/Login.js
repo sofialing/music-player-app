4a1d3e3a355a987cc355e2dc2a576b41ca7ca9a5
Sofia Ling <sofialing@live.se>
2020-12-31 10:55:47 +0100

    Updated login page.

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,13 +1,12 @@
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import bgImg from '../../assets/images/music-bg.png';
+import image from '../../assets/images/headphone.png';
 import { loginWithSpotify } from '../../spotify/login';
 
 const Login = () => {
 	return (
-		<main className="login-page" style={{ 'backgroundImage': `url(${bgImg})` }}>
-			<h1>Lorem ipsum dolor sit amet.</h1>
-			<p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-			<button className="btn btn-login" onClick={loginWithSpotify}><ExitToAppIcon /> Login with Spotify</button>
+		<main className="login-page" >
+			<img src={image} alt="" />
+			<button className="btn btn-login" onClick={loginWithSpotify}><ExitToAppIcon /> Connect with Spotify</button>
 		</main>
 	);
 }
