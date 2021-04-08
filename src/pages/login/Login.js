25f2046a577e0fee67ce9b2d963c0aabfe974e4a
Sofia Ling <sofialing@live.se>
2021-04-08 13:35:51 +0200

    Updated manifest and login page.

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,13 +1,11 @@
-import Headphones from 'pages/login/Headphones';
+import Headphones from 'pages/login/partials/Headphones';
 import MainView from 'components/views/MainView';
 
 const Login = () => {
-	const redirect = () => window.location.href = process.env.REACT_APP_BACKEND_URI;
-
 	return (
 		<MainView id="login" pageTitle="Log in">
 			<Headphones />
-			<button className="btn" onClick={redirect}>Log in</button>
+			<a className="btn" href={process.env.REACT_APP_BACKEND_URI}>Log in</a>
 			<small>Need an account? <a href="https://www.spotify.com/se/signup/">Sign up for free to start listening</a></small>
 		</MainView>
 	);
