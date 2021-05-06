a8477cd0a27e966df9662daea596a7dd17438c19
Sofia Ling <sofialing@live.se>
2021-05-06 16:38:10 +0200

    Added breakpoint object to utils.

---
+++ src/utils/breakpoints.js
@@ -0,0 +1,12 @@
+const breakpoints = {
+	// Small screen / phone
+	sm: 576,
+	// Medium screen / tablet
+	md: 768,
+	// Large screen / desktop
+	lg: 992,
+	// Extra large screen / wide desktop
+	xl: 1360,
+};
+
+export default breakpoints;
